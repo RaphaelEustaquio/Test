0104f9090ca509ff1b633b75090333f6e228cc8d
Raph <raffy211@gmail.com>
2022-11-08 10:09:17 -0800

    created code 2 increment a number by power of 2 till 1024 using while and for

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// alert("Click OK to receive your free PS5!");
+// console.log("Hello");
+
+for (i=1; i <= 1024; i*=2){
+    console.log(i)
+}
+
+// let i = 1;
+// while (i < 1024){
+//     i = i*2
+//     console.log(i)
+
+// }
